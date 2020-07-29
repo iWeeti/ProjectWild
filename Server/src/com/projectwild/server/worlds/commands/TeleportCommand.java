b854edee623a21b9268232f10f155f9c1d4fd0b1
WuXeR <axel.riska@gmail.com>
2020-07-30 00:01:10 +0300

    Fixed Teleport Command

--- Server/src/com/projectwild/server/worlds/commands/TeleportCommand.java
+++ Server/src/com/projectwild/server/worlds/commands/TeleportCommand.java
@@ -3,6 +3,7 @@ package com.projectwild.server.worlds.commands;
 import com.projectwild.server.WildServer;
 import com.projectwild.server.clients.Client;
 import com.projectwild.server.clients.Rank;
+import com.projectwild.server.worlds.World;
 import com.projectwild.server.worlds.players.Player;
 import com.projectwild.shared.packets.ChatMessagePacket;
 
@@ -10,24 +11,34 @@ public class TeleportCommand implements Command {
 
     @Override
     public void execute(Client client, String[] args) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String s : args)
-            stringBuilder.append(s);
-        Client to = null;
-        for (Player player : client.getPlayer().getWorld().getPlayers())
-            if (player.getNametag().toLowerCase().startsWith(stringBuilder.toString()))
-                to = player.getClient();
-        if (to == null) {
-            WildServer.getServer().sendToTCP(client.getSocket(), new ChatMessagePacket(String.format("[RED]Fail! [WHITE]Couldn't find the player %s.", stringBuilder.toString())));
+        World world = client.getPlayer().getWorld();
+
+        Client c = WildServer.getClientHandler().getClientByUsername(args[0]);
+        if(c == null) {
+            ChatMessagePacket packet = new ChatMessagePacket("[RED]Failed! [WHITE]Couldn't Find Player");
+            WildServer.getServer().sendToTCP(client.getSocket(), packet);
             return;
         }
-        client.getPlayer().getPosition().set(to.getPlayer().getPosition());
-        WildServer.getServer().sendToTCP(client.getSocket(), new ChatMessagePacket(String.format("[GREEN]Success! [WHITE]Teleported you to %s.", stringBuilder.toString())));
+
+        if(c.getPlayer() == null) {
+            ChatMessagePacket packet = new ChatMessagePacket("[RED]Failed! [WHITE]Couldn't Find Player");
+            WildServer.getServer().sendToTCP(client.getSocket(), packet);
+            return;
+        }
+
+        if(!c.getPlayer().getWorld().getName().equals(world.getName())) {
+            ChatMessagePacket packet = new ChatMessagePacket("[RED]Failed! [WHITE]Couldn't Find Player");
+            WildServer.getServer().sendToTCP(client.getSocket(), packet);
+            return;
+        }
+
+        client.getPlayer().updatePosition(c.getPlayer().getPosition(), true);
+        WildServer.getServer().sendToTCP(client.getSocket(), new ChatMessagePacket(String.format("[GREEN]Success! [WHITE]Teleported you to %s.", c.getUsername())));
     }
 
     @Override
     public String help() {
-        return "Teleports you to another player.";
+        return "Teleports you to another player";
     }
 
     @Override
